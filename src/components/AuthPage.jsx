073a9184cd8ebd0767ccde7cc14900352beee649
amitk-codes <amitkcodes@gmail.com>
2023-12-06 02:20:44 +0530

    first commit: created div structures of landing page

---
+++ src/components/AuthPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../stylesheets/auth_page.css"
+
+export const AuthPage = () => {
+  return(
+    <div className="auth-page-outer-div">
+      Hello from Auth Page
+    </div>
+  )
+}
---
+++ src/components/DetailsPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../stylesheets/details_page.css"
+
+export const DetailsPage = () => {
+  return (
+    <div className="width-100 details-page-outer-div">
+
+
+    </div>
+  )
+}
