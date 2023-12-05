a3ef734815683512d2aaa6370aebbccb25646c70
amitk-codes <amitkcodes@gmail.com>
2023-12-06 05:21:10 +0530

    changes in DetailsPage.jsx: imported notification and insight section

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "../stylesheets/details_page.css"
+import { NotificationSection } from "./notificationSection";
+import { InsightSection } from "./insightSection";
 
 export const DetailsPage = () => {
   return (
     <div className="width-100 details-page-outer-div">
+      <NotificationSection/>
+      <InsightSection/>
 
 
     </div>
